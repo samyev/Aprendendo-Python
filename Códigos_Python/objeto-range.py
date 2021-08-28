a6955e2eb1202fe79f9263cb6983608dc9938a5b
samlima <samlima@locus.software>
2021-08-28 18:20:33 -0300

    file: add arquivo objeto-range.py

---
+++ Códigos_Python/objeto-range.py
@@ -0,0 +1,13 @@
+#---------------------------------------------------------------------------
+# Em muitas línguagens de programação utilizamos o laço for para realizar 
+# iterações, então o python nos fornece o objeto range que simplifica essas
+# iterações
+#---------------------------------------------------------------------------
+
+# Utilizando range para imprimir uma frase 10 vezes
+for i in range(10):
+  print("Potatos!")
+
+# Imprimindo número pares de 0 a 10
+for i in range(0,10,2):
+  print(i)
